a65ac829d0c0f1cb65fe7b4708a65904323c51e0
9521Alex <alejandro.silva.casco@gmail.com>
2025-03-05 17:08:51 -0600

    Update alu_4bits.v

--- src/alu_4bits.v
+++ src/alu_4bits.v
@@ -4,7 +4,7 @@ module alu_4bits (
     input  [3:0] A,          // Operando A de 4 bits
     input  [3:0] B,          // Operando B de 4 bits
     input  [2:0] sel,        // Selector de operacion
-    output reg [3:0] result, // Resultado de la operacion
+    output reg [7:0] result, // Resultado de la operacion
     output reg carry,        // Indicador de carry/borrow en suma/resta
     output reg zero          // Bandera que indica resultado cero
 );
@@ -32,11 +32,15 @@ always @(*) begin
             // Operacion XOR
             result = A ^ B;
         end
-		
-        default: result = 4'b0000;
+	3'b101: begin
+            // Operacion multiplicacion
+            result = A * B;
+        end
+	    
+        default: result = 8'b00000000;
     endcase
     // Se activa la bandera zero si el resultado es 0
-    zero = (result == 4'b0000) ? 1'b1 : 1'b0;
+	zero = (result == 8'b00000000) ? 1'b1 : 1'b0;
 end
 
 endmodule
